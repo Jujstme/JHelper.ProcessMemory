dfe9155bfe1bcd1ada172c83295d1d8ddaa1cdfe
Jujstme <just.tribe@gmail.com>
2026-09-08 15:47:31 +0200

    Update endianness conversion functions

--- MemoryUtils/Endianness.cs
+++ MemoryUtils/Endianness.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Linq;
+using System.Buffers.Binary;
 using System.Runtime.CompilerServices;
 
 namespace JHelper.Common.MemoryUtils;
@@ -16,63 +16,96 @@ public static class Endian
     /// <param name="value">The value whose byte order will be converted.</param>
     /// <param name="endian">The desired endianness (either <see cref="Endianness.Little"/> or <see cref="Endianness.Big"/>).</param>
     /// <returns>The value with its byte order converted to the specified endianness.</returns>
-    public static T FromEndian<T>(this T value, Endianness endian) where T : unmanaged
-    {
-        bool isLittleEndian = BitConverter.IsLittleEndian;
-
-        // If the system is little-endian and desired endianness is also little, return the value as-is
-        if (isLittleEndian && endian == Endianness.Little)
-            return value;
-
-        // If the system is big-endian and the desired endianness is also big-endian, return the value as-is.
-        if (!isLittleEndian && endian == Endianness.Big)
-            return value;
-
-        // Otherwise, swap the byte order.
-        return SwapEndianness(value);
-    }
-
-    /// <summary>
-    /// Swaps the byte order (endianness) of an unmanaged type.
-    /// </summary>
-    /// <typeparam name="T">The unmanaged type whose byte order will be swapped.</typeparam>
-    /// <param name="value">The value whose byte order will be swapped.</param>
-    /// <returns>The value with its byte order swapped.</returns>
     [SkipLocalsInit]
-    public unsafe static T SwapEndianness<T>(this T value) where T : unmanaged
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T FromEndian<T>(T value, Endianness endian) where T : unmanaged
     {
-        int size = sizeof(T);
-
-        // If the size of the type is 1 byte, no swapping is needed (1 byte has no endianness).
-        if (size == 1)
+        // Return the value as-is if the system's current endianness matches the desired target endianness.
+        if (BitConverter.IsLittleEndian == (endian == Endianness.Little))
             return value;
 
-        // Allocate a buffer on the stack to store the byte representation of the value.
-        Span<byte> buffer = stackalloc byte[size];
+        var type = typeof(T);
 
-        // Create a span over the bytes of the value.
-        Span<byte> val = new(&value, size);
+        // 1-byte types require no swapping.
+        if (type == typeof(byte) || type == typeof(sbyte))
+            return value;
+        
+        // 2-byte types (short, ushort)
+        if (type == typeof(ushort) || type == typeof(short))
+        {
+            ushort temp = Unsafe.As<T, ushort>(ref value);
+            temp = BinaryPrimitives.ReverseEndianness(temp);
+            return Unsafe.As<ushort, T>(ref temp);
+        }
+        
+        // 4-byte types (int, uint, float)
+        if (type == typeof(uint) || type == typeof(int) || type == typeof(float))
+        {
+            uint temp = Unsafe.As<T, uint>(ref value);
+            temp = BinaryPrimitives.ReverseEndianness(temp);
+            return Unsafe.As<uint, T>(ref temp);
+        }
+        
+        // 8-byte types (long, ulong, double)
+        if (type == typeof(long) || type == typeof(ulong) || type == typeof(double))
+        {
+            ulong temp = Unsafe.As<T, ulong>(ref value);
+            temp = BinaryPrimitives.ReverseEndianness(temp);
+            return Unsafe.As<ulong, T>(ref temp);
+        }
 
-        // Copy the value's bytes to the buffer.
-        val.CopyTo(buffer);
-
-        // Reverse the bytes to swap the endianness.
-        buffer.Reverse();
-
-        // Convert the reversed bytes back to the original type and return the result.
-        fixed (void* pBuf = buffer)
-            return *(T*)pBuf;
+        // Restrict usage exclusively to supported primitives to protect against accidental custom struct corruption.
+        throw new InvalidOperationException("FromEndian<T> is supported only primitive types.");
     }
 
-    // Overloads for common unmanaged types
-    public static short FromEndian(this short value, Endianness endian) => value.FromEndian<short>(endian);
-    public static ushort FromEndian(this ushort value, Endianness endian) => value.FromEndian<ushort>(endian);
-    public static int FromEndian(this int value, Endianness endian) => value.FromEndian<int>(endian);
-    public static uint FromEndian(this uint value, Endianness endian) => value.FromEndian<uint>(endian);
-    public static long FromEndian(this long value, Endianness endian) => value.FromEndian<long>(endian);
-    public static ulong FromEndian(this ulong value, Endianness endian) => value.FromEndian<ulong>(endian);
-    public static float FromEndian(this float value, Endianness endian) => value.FromEndian<float>(endian);
-    public static double FromEndian(this double value, Endianness endian) => value.FromEndian<double>(endian);
+    // Common unmanaged types
+    [SkipLocalsInit]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static short FromEndian(this short value, Endianness endian) => BitConverter.IsLittleEndian == (endian == Endianness.Little) ? value : BinaryPrimitives.ReverseEndianness(value);
+    
+    [SkipLocalsInit]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ushort FromEndian(this ushort value, Endianness endian) => BitConverter.IsLittleEndian == (endian == Endianness.Little) ? value : BinaryPrimitives.ReverseEndianness(value);
+    
+    [SkipLocalsInit]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int FromEndian(this int value, Endianness endian) => BitConverter.IsLittleEndian == (endian == Endianness.Little) ? value : BinaryPrimitives.ReverseEndianness(value);
+    
+    [SkipLocalsInit]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static uint FromEndian(this uint value, Endianness endian) => BitConverter.IsLittleEndian == (endian == Endianness.Little) ? value : BinaryPrimitives.ReverseEndianness(value);
+    
+    [SkipLocalsInit]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static long FromEndian(this long value, Endianness endian) => BitConverter.IsLittleEndian == (endian == Endianness.Little) ? value : BinaryPrimitives.ReverseEndianness(value);
+    
+    [SkipLocalsInit]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ulong FromEndian(this ulong value, Endianness endian) => BitConverter.IsLittleEndian == (endian == Endianness.Little) ? value : BinaryPrimitives.ReverseEndianness(value);
+    
+    [SkipLocalsInit]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float FromEndian(this float value, Endianness endian)
+    {
+        if (BitConverter.IsLittleEndian == (endian == Endianness.Little))
+            return value; 
+
+        uint bits = Unsafe.As<float, uint>(ref value);
+        bits = BinaryPrimitives.ReverseEndianness(bits);
+        return Unsafe.As<uint, float>(ref bits);   
+    }
+
+    [SkipLocalsInit]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double FromEndian(this double value, Endianness endian)
+    {
+        if (BitConverter.IsLittleEndian == (endian == Endianness.Little))
+            return value; 
+
+        ulong bits = Unsafe.As<double, ulong>(ref value);
+        bits = BinaryPrimitives.ReverseEndianness(bits);
+        return Unsafe.As<ulong, double>(ref bits);   
+    }  
 
     /// <summary>
     /// Converts the byte order (endianness) of each element in a span of unmanaged types based on the desired endianness.
@@ -82,19 +115,12 @@ public static class Endian
     /// <param name="endian">The desired endianness (either <see cref="Endianness.Little"/> or <see cref="Endianness.Big"/>).</param>
     public static void FromEndian<T>(Span<T> value, Endianness endian) where T : unmanaged
     {
-        bool isLittleEndian = BitConverter.IsLittleEndian;
-
-        // If the system's endianness matches the desired endianness, no conversion is needed.
-        if (isLittleEndian && endian == Endianness.Little)
-            return;
-
-        // If the system is big-endian and desired endianness is big, return as-is
-        if (!isLittleEndian && endian == Endianness.Big)
+        if (BitConverter.IsLittleEndian == (endian == Endianness.Little))
             return;
 
         // Swap the byte order of each element in the span.
         for (int i = 0; i < value.Length; i++)
-            value[i] = value[i].SwapEndianness();
+            value[i] = FromEndian(value[i], endian);
     }
 }
 
